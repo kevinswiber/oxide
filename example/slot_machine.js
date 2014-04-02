0a20fe1df2938009edf7d45ee509bf3e8dbf3d8b
Kevin Swiber <kswiber@gmail.com>
2014-04-02 18:26:13 -0400

    Renaming variables in slot machine.

--- example/slot_machine.js
+++ example/slot_machine.js
@@ -1,20 +1,20 @@
 var readline = require('readline');
 var oxide = require('../');
 
-var coinEventSource = oxide.createEventSource();
-var playEventSource = oxide.createEventSource();
-var winEventSource = oxide.createEventSource();
-var rollEventSource = oxide.createEventSource();
-var mayPlayEventSource = oxide.createEventSource();
-var doesPlayEventSource = oxide.createEventSource();
-var deniedEventSource = oxide.createEventSource();
-var endEventSource = oxide.createEventSource();
+var coinEvents = oxide.createEventSource();
+var playEvents = oxide.createEventSource();
+var winEvents = oxide.createEventSource();
+var rollEvents = oxide.createEventSource();
+var mayPlayEvents = oxide.createEventSource();
+var doesPlayEvents = oxide.createEventSource();
+var deniedEvents = oxide.createEventSource();
+var endEvents = oxide.createEventSource();
 
-oxide.observe(mayPlayEventSource, function(val) {
+oxide.observe(mayPlayEvents, function(val) {
   if (val) {
-    doesPlayEventSource.emit();
+    doesPlayEvents.emit();
   } else {
-    deniedEventSource.emit();
+    deniedEvents.emit();
   }
 });
 
@@ -42,12 +42,12 @@ var loop = function() {
     switch (command) {
       case 'coin':
         last = 'coin';
-        coinEventSource.emit();
+        coinEvents.emit();
         loop();
         break;
       case 'play':
         last = 'play';
-        playEventSource.emit();
+        playEvents.emit();
         loop();
         break;
       case 'quit':
@@ -75,9 +75,9 @@ var loop = function() {
 };
 
 var credits = oxide.createVar(0);
-var creditsEventSource = coinEventSource.map(addCredit)
-  .merge(doesPlayEventSource.map(removeCredit))
-  .merge(winEventSource.map(addWin));
+var creditsEvents = coinEvents.map(addCredit)
+  .merge(doesPlayEvents.map(removeCredit))
+  .merge(winEvents.map(addWin));
 
 function addCredit() {
   credits.apply(credits.now() + 1);
@@ -99,56 +99,56 @@ function addWin(type) {
   return credits;
 };
 
-oxide.observe(playEventSource, function(val) {
-  mayPlayEventSource.emit((credits.now() > 0));
+oxide.observe(playEvents, function() {
+  mayPlayEvents.emit((credits.now() > 0));
 });
 
-var roll = function() {
+function roll() {
   return Math.floor(Math.random() * 7) + 1;
 };
 
-oxide.observe(doesPlayEventSource, function(val) {
-  rollEventSource.emit([roll(), roll(), roll()]);
+oxide.observe(doesPlayEvents, function(val) {
+  rollEvents.emit([roll(), roll(), roll()]);
 });
 
-oxide.observe(rollEventSource, function(val) {
+oxide.observe(rollEvents, function(val) {
   console.log('You rolled:', val[0], val[1], val[2]);
 });
 
-oxide.observe(rollEventSource, function(val) {
+oxide.observe(rollEvents, function(val) {
   var z1 = val[0]
   var z2 = val[1];
   var z3 = val[2];
   if (z1 == z2 && z2 == z3) {
-    winEventSource.emit('triple');
+    winEvents.emit('triple');
   } else if (z1 === z2 || z1 === z3 || z2 === z3) {
-    winEventSource.emit('double');
+    winEvents.emit('double');
   } else {
-    endEventSource.emit();
+    endEvents.emit();
   }
 });
 
-oxide.observe(coinEventSource, function() {
-  endEventSource.emit();
+oxide.observe(coinEvents, function() {
+  endEvents.emit();
 });
 
-oxide.observe(endEventSource, function() {
+oxide.observe(endEvents, function() {
   console.log('Credits:', credits.now());
 });
 
-oxide.observe(winEventSource, function(type) {
+oxide.observe(winEvents, function(type) {
   if (type === 'triple') {
     console.log('Wowwowow! A triple! So awesome!');
   } else if (type === 'double') {
       console.log('Wow, a double!');
   }
 
-  endEventSource.emit();
+  endEvents.emit();
 });
 
-oxide.observe(deniedEventSource, function(val) {
+oxide.observe(deniedEvents, function(val) {
   console.log('Not enough credits!');
-  endEventSource.emit();
+  endEvents.emit();
 });
 
 showHelp();
